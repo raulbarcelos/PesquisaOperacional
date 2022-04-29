62cf855e6112968a91736ea56c152117a081f1d3
Raul Barcelos <raul.barcelos@ufvjm.edu.br>
2022-04-29 17:40:27 -0300

    Resolvendo questão 04 - ano bissexto

---
+++ questoes/questao04.py
@@ -0,0 +1,44 @@
+
+import os
+import random
+
+os.system('cls' if os.name == 'nt' else 'clear')
+input("\nPressione Enter para iniciar... ")
+
+
+quant_pao_dormido=1.2 
+
+out=False
+while(not out):
+    
+        
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("**********************************************************************************")
+    print("********************************** EXERCICIO 04 **********************************")
+    print("**********************************************************************************")
+
+    ano=input("\nDigite o ano para descobrir se é bissexto: ")
+    if (not ano.isdigit()) or (not float(ano)%1 == 0):
+        print("**** Só é possivel digitar ano com numero inteiro.")
+    elif int(ano)==0:
+        print("**** Não é possivel digitar o valor 0 para o ano.")
+    else:
+        if(float(ano)%400==0):
+            print('O ano {} é bissexto.'.format(ano))
+        elif(float(ano)%100==0):
+            print('O ano {} NÃO é bissexto.'.format(ano))
+        elif(float(ano)%4==0):
+            print('O ano {} é bissexto.'.format(ano))
+        else:
+            print('O ano {} NÃO é bissexto.'.format(ano))
+
+
+    opcao=input("\nPressione Enter para digitar um novo ano ou 0 para sair: ")
+    if(opcao == 0 or opcao=='0'):
+        print("------ SAINDO DO PROGRAMA! ------")
+        out=True
+
+
+
+
+
